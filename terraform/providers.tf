a6597962788c16fd3ea9dc8aac51c996d693e782
mulyarch <yurmulyarchuk@gmail.com>
2026-05-18 14:47:24 -0700

    edited cy.yml

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.9.0"
 
   required_providers {
     aws = {
@@ -15,29 +15,8 @@ terraform {
       version = "~> 2.11"
     }
   }
-
-  backend "s3" {
-    bucket = "your-terraform-state-bucket"
-    key    = "eks-apache/terraform.tfstate"
-    region = "us-west-2"
-  }
 }
 
 provider "aws" {
   region = var.aws_region
 }
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
-
